3aa110c2bf13ce7bf5ddfd64b21f3061de649e08
Paul Gustafson <paul.gustafson@gmail.com>
2020-11-10 01:31:54 -0500

    map and mapMaybe

--- Partial.hs
+++ Partial.hs
@@ -4,34 +4,39 @@ module Partial where
 
 import qualified Data.Map as M
 import Control.Monad
-import Data.Maybe
+import qualified Data.Maybe as D
 
 
 class Partial f a where
-  map  :: (b -> Maybe c) -> f a b -> f a c
+  map :: (b -> c) -> f a b -> f a c
+  mapMaybe  :: (b -> Maybe c) -> f a b -> f a c
   ($) :: f a b -> a -> Maybe b
   (.)  :: Partial g b => g b c -> f a b -> f a c
-  g . f = Partial.map (g Partial.$) f
+  g . f = Partial.mapMaybe (g Partial.$) f
 
 instance (Ord k) => Partial M.Map k where
-  map = M.mapMaybe
+  map = M.map
+  mapMaybe = M.mapMaybe
   ($) = flip M.lookup
 
 newtype WList a b = WList { getList :: [b]} 
 instance Partial WList Int where
-  map f xs = WList (mapMaybe f (getList xs))
+  map f xs = WList (Prelude.map f (getList xs))
+  mapMaybe f xs = WList (D.mapMaybe f (getList xs))
   f $ n = if 0 <= n && n < length (getList f)
           then Just ((getList f) !! n)
           else Nothing
-         
+      
 
 newtype WMaybe a b = WMaybe { getMaybe :: Maybe b }
 instance Partial WMaybe () where
-  map f x =  WMaybe (f =<< (getMaybe x))
+  map f x = WMaybe (liftM f (getMaybe x))
+  mapMaybe f x =  WMaybe (f =<< (getMaybe x))
   x $ z = getMaybe x
 
 newtype WFun a b = WFun { getFun :: a -> Maybe b }
 instance Partial WFun a where
-   map g f  = WFun (g <=< (getFun f))
-   ($) = getFun
+  map g f  = WFun ((fmap g) Prelude.. (getFun f))
+  mapMaybe g f  = WFun (g <=< (getFun f))
+  ($) = getFun
 
